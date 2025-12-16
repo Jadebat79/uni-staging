28040146e71a2d5f07ae5523bdd22ca3de94cc80
Jade <jadebat79@gmail.com>
2025-12-16 15:32:09 +0000

    update

--- infra/aws/ecr.tf
+++ infra/aws/ecr.tf
@@ -1,10 +1,10 @@
-# --- 3. ECR Repositories (Add more apps here) ---
-resource "aws_ecr_repository" "app1" {
-  name         = "${var.project_name}/app1"
-  force_delete = true
-}
+# # --- 3. ECR Repositories (Add more apps here) ---
+# resource "aws_ecr_repository" "app1" {
+#   name         = "${var.project_name}/app1"
+#   force_delete = true
+# }
 
-resource "aws_ecr_repository" "app2" {
-  name         = "${var.project_name}/app2"
-  force_delete = true
-}
+# resource "aws_ecr_repository" "app2" {
+#   name         = "${var.project_name}/app2"
+#   force_delete = true
+# }
--- infra/aws/role.tf
+++ infra/aws/role.tf
@@ -1,24 +1,24 @@
-# --- 1. IAM Role (SSM Access) ---
-resource "aws_iam_role" "ssm_role" {
-  name = "${var.project_name}-ssm-role"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{ Action = "sts:AssumeRole", Effect = "Allow", Principal = { Service = "ec2.amazonaws.com" } }]
-  })
-}
+# # --- 1. IAM Role (SSM Access) ---
+# resource "aws_iam_role" "ssm_role" {
+#   name = "${var.project_name}-ssm-role"
+#   assume_role_policy = jsonencode({
+#     Version = "2012-10-17"
+#     Statement = [{ Action = "sts:AssumeRole", Effect = "Allow", Principal = { Service = "ec2.amazonaws.com" } }]
+#   })
+# }
 
-resource "aws_iam_role_policy_attachment" "ssm_core" {
-  role       = aws_iam_role.ssm_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
-}
+# resource "aws_iam_role_policy_attachment" "ssm_core" {
+#   role       = aws_iam_role.ssm_role.name
+#   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+# }
 
-# Allow EC2 to pull from ECR
-resource "aws_iam_role_policy_attachment" "ecr_read" {
-  role       = aws_iam_role.ssm_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-}
+# # Allow EC2 to pull from ECR
+# resource "aws_iam_role_policy_attachment" "ecr_read" {
+#   role       = aws_iam_role.ssm_role.name
+#   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+# }
 
-resource "aws_iam_instance_profile" "profile" {
-  name = "${var.project_name}-profile"
-  role = aws_iam_role.ssm_role.name
-}
+# resource "aws_iam_instance_profile" "profile" {
+#   name = "${var.project_name}-profile"
+#   role = aws_iam_role.ssm_role.name
+# }
