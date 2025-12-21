1da7c36513632f00dea3fc4cccf615c99045ed6f
Jade <jadebat79@gmail.com>
2025-12-21 10:29:07 +0000

    updates

--- infra/aws/sg.tf
+++ infra/aws/sg.tf
@@ -1,33 +1,33 @@
-resource "aws_security_group" "sg" {
-  name        = "${var.project_name}-sg"
-  description = "Staging Security Group"
+# resource "aws_security_group" "sg" {
+#   name        = "${var.project_name}-sg"
+#   description = "Staging Security Group"
 
-  # HTTP/HTTPS: Open to world for Let's Encrypt / Web Access
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+#   # HTTP/HTTPS: Open to world for Let's Encrypt / Web Access
+#   ingress {
+#     from_port   = 80
+#     to_port     = 80
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+#   ingress {
+#     from_port   = 443
+#     to_port     = 443
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
 
-  # SSH: Restricted
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = [var.allowed_cidr]
-  }
+#   # SSH: Restricted
+#   ingress {
+#     from_port   = 22
+#     to_port     = 22
+#     protocol    = "tcp"
+#     cidr_blocks = [var.allowed_cidr]
+#   }
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
+#   egress {
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+# }
