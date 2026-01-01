164ce4a7bae0894b62d111d739fefda598f67575
Jade <jadebat79@gmail.com>
2026-01-01 18:04:44 +0000

    new updates

--- infra/aws/compute.tf
+++ infra/aws/compute.tf
@@ -1,28 +1,28 @@
-# # --- 4. EC2 Instance ---
-# resource "aws_instance" "server" {
-#   ami                  = "ami-0ecb62995f68bb549" # Ubuntu 22.04 LTS (Update for your region!)
-#   instance_type        = "t3.small"
-#   iam_instance_profile = aws_iam_instance_profile.profile.name
-#   vpc_security_group_ids = [aws_security_group.sg.id]
+# --- 4. EC2 Instance ---
+resource "aws_instance" "server" {
+  ami                  = "ami-0ecb62995f68bb549" # Ubuntu 22.04 LTS (Update for your region!)
+  instance_type        = "t3.small"
+  iam_instance_profile = aws_iam_instance_profile.profile.name
+  vpc_security_group_ids = [aws_security_group.sg.id]
 
-#   # Inject variables into the boot script
-#   user_data = templatefile("${path.module}/user_data.sh", {
-#     git_token    = var.github_token
-#     git_repo     = replace(var.github_repo_url, "https://", "")
-#     project_name = var.project_name
-#     aws_region   = var.region
-#     domain_name  = var.domain_name
-#     office_ip    = var.office_ip
-#     ecr_url      = "${var.aws_account_id}.dkr.ecr.${var.region}.amazonaws.com"
-#   })
+  # Inject variables into the boot script
+  user_data = templatefile("${path.module}/user_data.sh", {
+    git_token    = var.github_token
+    git_repo     = replace(var.github_repo_url, "https://", "")
+    project_name = var.project_name
+    aws_region   = var.region
+    domain_name  = var.domain_name
+    office_ip    = var.office_ip
+    ecr_url      = "${var.aws_account_id}.dkr.ecr.${var.region}.amazonaws.com"
+  })
 
-#   tags = { 
-#     Name = "${var.project_name}-box" 
-#     }
-# }
+  tags = { 
+    Name = "${var.project_name}-box" 
+    }
+}
 
-# # --- 4. Elastic IP ---
-# resource "aws_eip" "lb" {
-#   instance = aws_instance.server.id
-#   domain   = "vpc"
-# }
+# --- 4. Elastic IP ---
+resource "aws_eip" "lb" {
+  instance = aws_instance.server.id
+  domain   = "vpc"
+}
