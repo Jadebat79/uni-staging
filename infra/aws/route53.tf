5432a3c443c927da54479d777b2e049e0068d4e0
Jade <jadebat79@gmail.com>
2026-01-01 18:46:57 +0000

    updates

--- infra/aws/route53.tf
+++ infra/aws/route53.tf
@@ -3,13 +3,30 @@ data "aws_route53_zone" "main" {
   name = "teamcanvas.site." # Your root domain
 }
 
-resource "aws_route53_record" "staging_wildcard" {
-  zone_id = data.aws_route53_zone.main.zone_id
-  name    = "*.staging"  # The wildcard for all staging subdomains
-  type    = "A"
-  ttl     = "300"
+# resource "aws_route53_record" "dozzle" {
+#   zone_id = data.aws_route53_zone.main.zone_id
+#   name    = "logs.staging"  # The wildcard for all staging subdomains
+#   type    = "A"
+#   ttl     = "300"
   
-  # This implies: "Look at the EIP resource and put its IP address here"
-  records = [aws_eip.lb.public_ip] 
-}
+#   # This implies: "Look at the EIP resource and put its IP address here"
+#   records = [aws_eip.lb.public_ip] 
+# }
 
+# resource "aws_route53_record" "backend" {
+#   zone_id = data.aws_route53_zone.main.zone_id
+#   name    = "gpc-api.staging"  # The wildcard for all staging subdomains
+#   type    = "A"
+#   ttl     = "300"
+  
+#   # This implies: "Look at the EIP resource and put its IP address here"
+#   records = [aws_eip.lb.public_ip] 
+# }
+
+# resource "aws_route53_record" "backend" {
+#   zone_id = data.aws_route53_zone.main.zone_id
+#   name    = "gpc-api.staging"  # The wildcard for all staging subdomains
+#   type    = "A"
+#   ttl     = "300"
+
+#   # This implies: "Look at the EIP resource and put its IP address here"
