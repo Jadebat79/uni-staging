700536e08d0b734dac162e1e13b1c8ed4680d4bc
Jade <jadebat79@gmail.com>
2026-01-01 17:55:22 +0000

    push

--- infra/aws/route53.tf
+++ infra/aws/route53.tf
@@ -3,13 +3,13 @@ data "aws_route53_zone" "main" {
   name = "teamcanvas.site." # Your root domain
 }
 
-resource "aws_route53_record" "staging_wildcard" {
-  zone_id = data.aws_route53_zone.main.zone_id
-  name    = "*.staging"  # The wildcard for all staging subdomains
-  type    = "A"
-  ttl     = "300"
+# resource "aws_route53_record" "staging_wildcard" {
+#   zone_id = data.aws_route53_zone.main.zone_id
+#   name    = "*.staging"  # The wildcard for all staging subdomains
+#   type    = "A"
+#   ttl     = "300"
   
-  # This implies: "Look at the EIP resource and put its IP address here"
-  records = [aws_eip.lb.public_ip] 
-}
+#   # This implies: "Look at the EIP resource and put its IP address here"
+#   records = [aws_eip.lb.public_ip] 
+# }
 
